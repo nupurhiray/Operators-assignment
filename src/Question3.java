9a84640a826cc189a48455599a26a5d6ec66d387
Nupur Dinesh Hiray <its.nupurhiray@gmail.com>
2023-09-29 22:32:16 +0530

    Initial commit

---
+++ src/Question3.java
@@ -0,0 +1,14 @@
+public class Question3
+{
+    public static void main(String args[])
+    {
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+    }
+
+    //Output is 4 0 0
+}
